60208576a9094e833159005927796e9a22de67ae
Sergey Suprunenko <suprunenko.s@gmail.com>
2015-04-04 11:56:08 +0300

    [add] Change directory order

---
+++ lib/omniauth-shutterstock-contributor.rb
@@ -0,0 +1,2 @@
+require 'omniauth-shutterstock-contributor/version'
+require 'omniauth/strategies/shutterstock/contributor'
--- lib/omniauth/shutterstock/contributor.rb
+++
@@ -1,9 +0,0 @@
-require "omniauth/shutterstock/contributor/version"
-
-module Omniauth
-  module Shutterstock
-    module Contributor
-      # Your code goes here...
-    end
-  end
-end
